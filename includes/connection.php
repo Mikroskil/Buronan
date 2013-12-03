5cfd01314c6273c6b56c77353ce0ee2a70c199b8
Tarigan <brian_efraim@yahoo.co.id>
2013-12-03 13:54:12 +0700

    koneksi database

---
+++ includes/connection.php
@@ -0,0 +1,8 @@
+<?php
+   
+    //membuat koneksi dengan mysql
+		mysql_connect('localhost','root','') or die ("koneksi gagal !!!");
+	//membuat koneksi dengan database
+		mysql_select_db('tracerstudy');
+
+?>
