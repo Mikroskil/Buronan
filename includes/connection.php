42ce83fcaf1c76f36c36806e223aa9cb8a5fcfdd
Tarigan <brian_efraim@yahoo.co.id>
2013-12-03 14:00:18 +0700

    index

--- includes/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-   
-    //membuat koneksi dengan mysql
-		mysql_connect('localhost','root','') or die ("koneksi gagal !!!");
-	//membuat koneksi dengan database
-		mysql_select_db('tracerstudy');
-
-?>
---
+++ index.php
@@ -0,0 +1,125 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <title>Mikroskil Graduates</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <script src="js/jquery-1.9.1.min.js"></script>
+    <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->
+     
+    <!-- Custom styles for this template -->
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+  
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+
+<style type="text/css">
+      .carousel {
+       
+        overflow: hidden;
+      }
+      .carousel .item {
+        -webkit-transition: opacity 1s;
+        -moz-transition: opacity 1s;
+        -ms-transition: opacity 1s;
+        -o-transition: opacity 1s;
+        transition: opacity 1s;
+      }
+      .carousel .active.left, .carousel .active.right {
+        left:0;
+        opacity:0;
+        z-index:2;
+      }
+      .carousel .next, .carousel .prev {
+        left:0;
+        opacity:1;
+        z-index:1;
+      }
+    </style>
+
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.carousel').carousel({interval: 6500});
+  });
+</script>
+
+
+  </head>
+
+  <body>
+
+    <!-- Wrap all page content here -->
+    <div id="wrap">
+
+<?php include('includes/nav.php'); ?>
+
+<?php include('includes/header.php'); ?>
+
+        <div class="breadcrumb">
+        </div>
+        
+       <?php include('includes/partisipasi.php'); ?>  
+       	
+    <div class="well">    
+    <div class="row">
+        <div class="col-lg-4">
+          <h2>  <img data-src="" src="images/tracerstudy.jpg" class="img-thumbnail" alt=""></h2>
+          <p><b>AYO… BERPARTISIPASI PADA TRACER STUDY MIKROSKIL</b></p>
+          <p align="justify">Tracer Study adalah studi penelusuran alumni untuk mengetahui kegiatan alumni setelah lulus dari Perguruan Tinggi, transisi dari dunia pendidikan tinggi ke dunia kerja, situasi kerja, pemerolehan kompetensi kompetensi dan penggunaan kompetensi dalam  </p>
+          <p><a class="btn btn-primary" href="news1.php?p=Berita_Satu">Read more &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+          <h2>  <img data-src="" src="images/expo.jpg" class="img-thumbnail" alt=""></h2>
+          <p><b>Iklan Mikroskil Career Expo 2013 Web Mikroskil</b></p>
+          <p align="justify">Sedang Mencari Pekerjaan??? Atau Ingin Pindah Kerja???
+Hadiri serta bawa surat lamaran Anda pada kegiatan. Mikroskil Career Expo 2013
+Jumat - Sabtu, 18-19 Oktober 2013
+Pkl. 09.00 s/d 16.00 Wib
+Hall Kampus C Lantai 5 Mikroskil
+Jl. Thamrin No. 112 Medan </p>
+          <p><a class="btn btn-primary" href="news2.php?p=Berita_Dua">Read more &raquo;</a></p>
+       </div>
+        <div class="col-lg-4">
+          <h2>  <img data-src="" src="images/hackathon.jpg" class="img-thumbnail" alt=""></h2>
+          <p><b>StartUp Basecamp dan Hack-a-thon oleh UKM MPC</b></p>
+          <p align="justify">StartUp BaseCamp merupakan inisiatif UKM Mikroskil Programming Club (MPC) untuk memberikan pengetahuan membangun startup di bidang aplikasi web. StartUp BaseCamp ini akan dilaksanakan selama satu minggu secara full-time dari pukul 09.00-17.00 .</p>
+          <p><a class="btn btn-primary" href="news3.php?p=Berita_Tiga">Read more &raquo;</a></p>
+        </div>
+      </div>
+      </div>
+
+        
+        <br>
+                 
+        <!--<p class="lead">All graduates can fill up Tracer Study form. <br /> 
+        Login by entering your User ID and Password <br />
+        User ID : <code>NIM</code> <br />
+        Password : <code>dd/MM/yyyy</code> 
+        <p>For more information, please send an email to admin@alumnimikroskil.com</address> </p>-->
+      </div>
+    </div>
+
+   <?php include('includes/footer.php'); ?>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+   	<script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/holder.js"></script>
+  </body>
+</html>
